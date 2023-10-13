e48a22e72af5816559863412cd8d0148e979cbc1
codemsbr <147555728+codemsbr@users.noreply.github.com>
2023-10-12 18:59:59 -0700

    Add files via upload

---
+++ Task4/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Task 1
+            //int lasNumber = 0;
+            //int nexNumber = 1;
+            //int fibNumber = 0;
+            //int number = 12;
+            //while (fibNumber >= number)
+            //{
+            //    Console.Write(lasNumber + " ");
+            //    fibNumber = lasNumber + nexNumber;
+            //    lasNumber = nexNumber;
+            //    nexNumber = fibNumber;
+            //}
+            #endregion
+
+            #region Task 2
+            int numberOne = 14;
+            int numberTwo = 9;
+
+            numberOne = numberOne + numberTwo;
+            numberTwo = numberOne - numberTwo;
+            numberOne = numberOne - numberTwo;
+            Console.WriteLine(numberOne);
+            Console.WriteLine(numberTwo);
+            #endregion 
+        }
+    }
+}
